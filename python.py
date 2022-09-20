c57abcedbb50e6bfff5ad4a5877e30435025bbe2
minwoo <minwoo02100@naver.com>
2022-09-20 11:20:41 +0900

    bot token set

--- python.py
+++ python.py
@@ -1,21 +1,23 @@
-#run.py
+# run.py
 import discord
-from token import Token
+import asyncio
+
+from to import Token
 
 intents = discord.Intents.all()
-# intents.typing = True
-# intents.presences = True
-# intents.members = True
 client = discord.Client(intents=intents)
+
 
 @client.event
 async def on_ready():
     print('{0.user} 이 준비되었습니다!'.format(client))
-    await client.change_presence(status=discord.Status.online, activity=discord.Game("")) # 상태메시지 표시
+    # 상태메시지 표시
+    await client.change_presence(status=discord.Status.online, activity=discord.Game("국어"))
+
 
 @client.event
 async def on_message(message):
-    if message.author == client.user: 
+    if message.author == client.user:
         # 봇 자신이 보내는 메세지는 무시
         return
 
@@ -23,4 +25,5 @@ async def on_message(message):
         await message.channel.send('안녕!')
 
 
-client.run('Token') # 보안상 다른 코드에서 토큰값을 가져옴
+# 보안상 다른 파일에서 토큰값을 가져옴
+client.run(Token)
