5717526500dc7dd9423039440a4bc687d71cf30f
minwoo <minwoo02100@naver.com>
2022-09-23 16:49:57 +0900

    v1 miku

--- python.py
+++ python.py
@@ -1,4 +1,6 @@
 # run.py
+from socket import MsgFlag
+from ssl import CHANNEL_BINDING_TYPES
 import discord
 import asyncio
 
@@ -12,7 +14,7 @@ client = discord.Client(intents=intents)
 async def on_ready():
     print('{0.user} 이 준비되었습니다!'.format(client))
     # 상태메시지 표시
-    await client.change_presence(status=discord.Status.online, activity=discord.Game("국어"))
+    await client.change_presence(status=discord.Status.online, activity=discord.Game(""))
 
 
 @client.event
@@ -22,7 +24,12 @@ async def on_message(message):
         return
 
     if message.content == '안녕':
-        await message.channel.send('안녕!')
+        channel = message.channel
+
+        await message.channel.send('https://pbs.twimg.com/media/FcnWOQaaEAAU318.jpg')
+        await message.channel.send(f'안녕 {message.author.name}!\n여기는 세카이야\n너의 마음으로 이루어진 곳이지!')
+        await asyncio.sleep(6)
+        await message.channel.send('응.')
 
 
 # 보안상 다른 파일에서 토큰값을 가져옴
